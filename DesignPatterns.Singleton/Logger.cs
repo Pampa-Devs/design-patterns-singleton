637a1d568f188275562e9a0d30f2d907abde77a3
felipe-allmeida <felipe.allmeida.dev@gmail.com>
2020-05-14 02:42:02 -0300

    hello world

---
+++ DesignPatterns.Singleton/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Singleton
+{
+    public class Logger
+    {
+        private static Logger _instance;
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void Log(string message, ConsoleColor consoleColor = ConsoleColor.White)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger.Instance.Log("Hello World", ConsoleColor.Green);
+
+            Test.Init();
+
+            SoundService soundService = new SoundService();
+
+            soundService.PlaySound("Foo Fighters");
+        }
+    }
+
+    class Test
+    {
+        public static void Init()
+        {
+            Logger.Instance.Log("Hello from another world", ConsoleColor.Red);
+        }
+    }
+
+    public class SoundService
+    {
+        public void PlaySound(string sound)
+        {
+            // lógica para tocar o som       
+            
+            Logger.Instance.Log("Som iniciado" + sound);
+        }
+    }
+}
